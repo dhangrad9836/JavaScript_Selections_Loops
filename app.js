c8f8d4cb45401f5a88963df1cf63b9b2118bac21
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-04-08 09:48:27 -0400

    finished exercise 1 & 2

--- app.js
+++ app.js
@@ -2,6 +2,36 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for (let i = 1; i <= 100; i++) {
+  //console.log(i);
+  if (i % 2 == 1) {
+    console.log(i);
+  }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let i = 1; i <= 100; i++) {
+  //console.log(i);
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(`${i}.."FIZZBUZZ"`);
+  } else if (i % 3 == 0) {
+    console.log(`${i}.."FIZZ"`);
+  } else if (i % 5 == 0) {
+    console.log(`${i}.."BUZZ`);
+  }
+}
+
+// Exercise 3 Section
+//ex 1
+let i = 1;
+while (i <= 100) {
+  console.log(i);
+  i++;
+}
+//do while loop
+let x = 1;
+do {
+  console.log(x);
+  x++;
+} while (x <= 100);
