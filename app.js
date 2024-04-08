6eb59149ef56fb8eb4db193afb8a33ebadbe4edf
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-04-08 10:24:05 -0400

    Completed bonus exercise

--- app.js
+++ app.js
@@ -74,3 +74,20 @@ for (i = 0; i <= n; i++) {
     console.log(`Did not find value`);
   }
 }
+
+//exercise 5
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let start = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let end = Math.round(Math.random() * (1000 - 100) + 100); // creates a random number between 100 and 1000
+
+for (let i = start; i <= end; i++) {
+  //console.log(i);
+  if (i % fizzDivisor == 0 && i % buzzDivisor == 0) {
+    console.log(`${i}.."FIZZBUZZ"`);
+  } else if (i % fizzDivisor == 0) {
+    console.log(`${i}.."FIZZ"`);
+  } else if (i % buzzDivisor == 0) {
+    console.log(`${i}.."BUZZ`);
+  }
+}
